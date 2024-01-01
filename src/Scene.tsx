d00ed6a2f6146137f03dad08a1f7d2f0436100c7
Eric <ericrius1@gmail.com>
2024-01-01 16:29:01 -0300

    cleanse

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -5,23 +5,10 @@ import { Perf } from 'r3f-perf'
 import { useRef } from 'react'
 import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three'
 import * as THREE from 'three'
-import { Frame } from './components/Frame'
-import { DeepArt } from './components/DeepArt'
-import { Plane } from './components/Plane'
 
 function Scene() {
   const { performance } = useControls('Monitoring', {
     performance: false,
-  })
-
-  const { animate } = useControls('Cube', {
-    animate: true,
-  })
-
-  const cubeRef = useRef<Mesh<BoxGeometry, MeshBasicMaterial>>(null)
-
-  useFrame((_, delta) => {
-
   })
 
   return (
@@ -29,41 +16,12 @@ function Scene() {
       <OrbitControls makeDefault />
       {performance && <Perf position='top-left' />}
 
-
-      <directionalLight
-        position={[-2, 2, 3]}
-        intensity={1.5}
-        castShadow
-        shadow-mapSize={[1024 * 2, 1024 * 2]}
-      />
-      <ambientLight intensity={0.2} />
-
-
-      <Frame id="0" name="alchemist" >
-        <Sky />
-        <Model position={[0, -2, 0]} />
-      </Frame >
-      <Model clip position={[0, -2, 0]} />
-      <Plane />
-
-      {/* <Frame id="02" name="dinasour" >
-        <DeepArt imagePath="/image.jpg" depthMapPath="/depth.jpg" />
-      </Frame> */}
+      <mesh>
+        <meshBasicMaterial color='purple' />
+        <boxGeometry />
+      </mesh>
     </>
   )
 }
-
-const zPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0)
-const yPlane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 1)
-
-function Model({ clip = false, ...props }) {
-  const { nodes, materials } = useGLTF('/mccree.glb')
-  return (
-    <mesh geometry={(nodes.base as THREE.Mesh).geometry} {...props} dispose={null}>
-      <meshBasicMaterial map={(materials.PaletteMaterial001 as THREE.MeshBasicMaterial).map} side={THREE.DoubleSide} clippingPlanes={clip ? [zPlane, yPlane] : null} />
-    </mesh>
-  )
-}
-
 
 export { Scene }
