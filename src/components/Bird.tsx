46fc75e0f61476ff6bdec22fb381bf150a56b0ea
Eric <ericrius1@gmail.com>
2024-01-01 22:32:11 -0300

    better occlusion

--- src/components/Bird.tsx
+++ src/components/Bird.tsx
@@ -3,6 +3,8 @@ import { useEffect, useLayoutEffect, useRef } from 'react'
 import * as THREE from 'three'
 import { useFrame } from '@react-three/fiber'
 
+const testMat = new THREE.MeshStandardMaterial()
+
 export function Bird() {
   const { scene, animations } = useGLTF('/models/macaw.glb')
   const { actions, ref } = useAnimations(animations, scene)
@@ -14,7 +16,12 @@ export function Bird() {
   useLayoutEffect(() => {
     scene.traverse((child) => {
       if ((child as any).isMesh) {
-        child.material.depthWrite = false
+        console.log(child.material)
+        child.frustumCulled = false
+        // child.material = testMat
+        // child.material.depthWrite = false
+        child.material.transparent = true
+        // debugger
         child.material.side = THREE.DoubleSide
       }
     })
