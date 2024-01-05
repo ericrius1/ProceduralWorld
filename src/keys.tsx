f42d43bc06fb2c2b57940e4466f3d6b45dc798c7
Eric <ericrius1@gmail.com>
2024-01-05 12:29:04 -0300

    adding store

---
+++ src/keys.tsx
@@ -0,0 +1,2 @@
+export const keys = <T extends Record<PropertyKey, unknown>>(o: T): (keyof T)[] =>
+  Object.keys(o)
---
+++ src/ui/Intro.tsx
@@ -0,0 +1,58 @@
+import { Suspense, useEffect, useState } from 'react'
+import { Footer } from '@pmndrs/branding'
+import { useProgress } from '@react-three/drei'
+
+import type { ReactNode } from 'react'
+
+import { useStore } from '../store'
+import { setupSession } from '../data'
+import { Keys } from './Keys'
+
+export function Intro({ children }: { children: ReactNode }): JSX.Element {
+  const [clicked, setClicked] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const { progress } = useProgress()
+  const [session, set] = useStore((state) => [state.session, state.set])
+
+  useEffect(() => {
+    if (clicked && !loading) set({ ready: true })
+  }, [clicked, loading])
+
+  useEffect(() => {
+    if (progress === 100) setLoading(false)
+  }, [progress])
+
+  useEffect(() => {
+    setupSession(set)
+  }, [])
+
+  return (
+    <>
+      <Suspense fallback={null}>{children}</Suspense>
+      <div
+        className={`fullscreen bg ${loading ? 'loading' : 'loaded'} ${
+          clicked && 'clicked'
+        }`}
+      >
+        <div className='stack'>
+          <div className='intro-keys'>
+            <Keys style={{ paddingBottom: 20 }} />
+            <a className='start-link' href='#' onClick={() => setClicked(true)}>
+              {loading ? `loading ${progress.toFixed()} %` : 'Click to start'}
+            </a>
+          </div>
+
+          <div>Hola Eric, ¿estás listo para aprender algo de español?</div>
+        </div>
+        <Footer
+          date='2. June'
+          year='2021'
+          link1={
+            <a href='https://github.com/pmndrs/react-three-fiber'>@react-three/fiber</a>
+          }
+          link2={<a href='https://github.com/pmndrs/racing-game'>/racing-game</a>}
+        />
+      </div>
+    </>
+  )
+}
