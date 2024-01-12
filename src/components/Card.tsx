f0d178db1821b2e60f0c5c52154f20f7ec2be036
Eric <ericrius1@gmail.com>
2024-01-12 09:50:05 -0300

    remove await from Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,7 @@
 import { Html } from '@react-three/drei'
-
-const res = await fetch('/spanish.json')
-const data = await res.json()
-
 export function Card({ ...props }) {
   return (
     <Html {...props} transform>
-      {/* <div className='card'>{data.Conversational.Spanish[0]}</div> */}
       <div className='card'>Fly. Fly</div>
     </Html>
   )
